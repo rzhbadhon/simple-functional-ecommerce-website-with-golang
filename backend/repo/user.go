81fcf47dc50308998314695758b1e6cf3f40fe86
rzhbadhon <rhbadhon030@gmail.com>
2025-10-08 23:43:51 +0600

    Updated product handler and repo-->user

--- backend/repo/user.go
+++ backend/repo/user.go
@@ -1,38 +1,43 @@
 package repo
 
-type User struct{
-	ID int `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
-	Email string `json:"email"`
-	Password string `json:"password"`
-	IsShopOwner bool `json:"is_shop_owner"`
+type User struct {
+	ID          int    `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	IsShopOwner bool   `json:"is_shop_owner"`
 }
 
-type UserRepo interface{
-	Create(p Product) (*Product, error)
-	Get(productID int) (*Product, error)
-	List() ([]*Product, error)
-	Delete(productID int) error
-	Update(p Product) (*Product, error)
+type UserRepo interface {
+	Create(user User) (*User, error)
+	Find(email, pass string) (*User, error)
+	/* List() ([]*User, error)
+	Delete(UserID int) error
+	Update(user User) (*User, error) */
 }
 
+type userRepo struct {
+	users []User
+}
 
-var users []User
+func NewUserRepo() UserRepo {
+	return &userRepo{}
+}
 
-func(u User) Store() User{
-	if u.ID != 0{
-		return u
+func (r *userRepo) Create(user User) (*User, error) {
+	if user.ID != 0 {
+		return &user, nil
 	}
-	u.ID = len(users)+1
-	users = append(users, u)
-	return u
+	user.ID = len(r.users) + 1
+	r.users = append(r.users, user)
+	return &user, nil
 }
-func Find(email, pass string) (*User){
-	for _, u := range users{
-		if u.Email == email && u.Password == pass{
-			return &u
+func (r *userRepo) Find(email, pass string) (*User, error) {
+	for _, u := range r.users {
+		if u.Email == email && u.Password == pass {
+			return &u, nil
 		}
 	}
-	return nil
+	return nil, nil
 }
--- backend/rest/handlers/product/create_product.go
+++ backend/rest/handlers/product/create_product.go
@@ -1,15 +1,27 @@
 package product
 
 import (
-	"ecommerce/database"
+	"ecommerce/repo"
 	"ecommerce/util"
 	"encoding/json"
 	"net/http"
 )
 
+
+//    01:31:00
+
+
+type ReqCreatProduct struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"imageUrl"`
+} 
+
 func (h *Handler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 
-	var newProduct database.Product
+	var newProduct repo.Product
 
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newProduct)
@@ -19,8 +31,11 @@ func (h *Handler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	createdProduct := database.Store(newProduct)
+	createdProduct, err := h.productRepo.Create(newProduct) //database.Store(newProduct)
+	if err != nil{
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 
-	util.SendData(w, createdProduct, 201)
+	util.SendData(w, createdProduct, http.StatusOK)
 
 }
