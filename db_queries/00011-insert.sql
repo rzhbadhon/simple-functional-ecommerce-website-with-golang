8eee449967866fc27166b8206aa1c86d10e09d33
rzhbadhon <rhbadhon030@gmail.com>
2025-10-10 21:40:18 +0600

    added database queries

---
+++ db_queries/00011-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users(
+	first_name,
+	last_name,
+	email,
+	password
+) VALUES(
+	'Razibul Hasan',
+	'Badhon',
+	'badhon@go.com',
+	'12345'
+)
---
+++ db_queries/00013-update.sql
@@ -0,0 +1,5 @@
+UPDATE users SET 
+	last_name = 'Nobita',
+	first_name = 'Noob'
+WHERE
+	id=2;
