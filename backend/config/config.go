51f89a1339cf3952fad4550d6673c592ae099c3b
rzhbadhon <rhbadhon030@gmail.com>
2025-10-11 00:21:07 +0600

    configured .env with databased

--- backend/config/config.go
+++ backend/config/config.go
@@ -10,12 +10,21 @@ import(
 
 var configurations *Config
 
+type DBConfig struct{
+	Host string
+	Port int
+	Name string
+	User string
+	Password string
+	EnableSSLMODE bool
+}
 
 type Config struct{
 	Version string
 	ServiceName string
 	HttpPort int
 	JwtSecretKey string
+	DB *DBConfig
 }
 
 func loadConfig(){
@@ -57,11 +66,66 @@ func loadConfig(){
 		os.Exit(1)
 	}
 
+
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == ""{
+		fmt.Println("DB Host is required")
+		os.Exit(1)
+	}
+	
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == ""{
+		fmt.Println("DB Port is required")
+		os.Exit(1)
+	}
+
+	dbPrt, err := strconv.ParseInt(dbPort, 10, 64)
+	if err != nil{
+		fmt.Println("DB Port must be a number")
+		os.Exit(1)
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == ""{
+		fmt.Println("Database name is required")
+		os.Exit(1)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == ""{
+		fmt.Println("DB User name is required")
+		os.Exit(1)
+	}
+
+	dbPass := os.Getenv("DB_PASSWORD")
+	if dbPass == ""{
+		fmt.Println("DB Password is required")
+		os.Exit(1)
+	}
+
+	enableSslMode := os.Getenv("DB_ENABLE_SSL_MODE")
+
+	sslMode, err := strconv.ParseBool(enableSslMode)
+	if err != nil{
+		fmt.Println("DB SSL MODE is required")
+		os.Exit(1)
+	}
+	
+	dbConfig := &DBConfig{
+		Host: dbHost,
+		Port: int(dbPrt),
+		Name: dbName,
+		User: dbUser,
+		Password: dbPass,
+		EnableSSLMODE: sslMode,
+	}
+
 	configurations = &Config{
 		Version: version,
 		ServiceName: serviceName,
 		HttpPort: int(port),
 		JwtSecretKey: jwtSecretKey,
+		DB: dbConfig,
 	}
 
 }
--- backend/infra/db/connection.go
+++ backend/infra/db/connection.go
@@ -1,19 +1,34 @@
 package db
 
 import (
+	"ecommerce/config"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-func GetConnectionString() string{
+func GetConnectionString(cnf *config.DBConfig) string{
+	connString := fmt.Sprintf(
+		"user=%s password=%s host=%s port=%d dbname=%s sslmode=%s",
+		cnf.User,
+		cnf.Password,
+		cnf.Host,
+		cnf.Name,
+		cnf.Port,
+		cnf.Name,
+	)
+
+	if !cnf.EnableSSLMODE{
+		connString += " sslmode=disable"
+	}
+
 	return "user=postgres password=1212 host=localhost port=5432 dbname=ecommerce sslmode=disable"
 }
 
 
-func NewConnection() (*sqlx.DB, error){
-	dbSource := GetConnectionString()
+func NewConnection(cnf *config.DBConfig) (*sqlx.DB, error){
+	dbSource := GetConnectionString(cnf)
 
 	dbCon, err := sqlx.Connect("postgres", dbSource)
 	if err != nil{
