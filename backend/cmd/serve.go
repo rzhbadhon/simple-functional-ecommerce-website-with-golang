29628bb733d2eecfb6cd65ff726a2b337d1d85b9
rzhbadhon <rhbadhon030@gmail.com>
2025-10-10 21:25:07 +0600

    Added PostgreSQL integration

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -2,18 +2,27 @@ package cmd
 
 import (
 	"ecommerce/config"
+	"ecommerce/infra/db"
 	"ecommerce/repo"
 	"ecommerce/rest"
 	"ecommerce/rest/handlers/product"
 	"ecommerce/rest/handlers/user"
 	middleware "ecommerce/rest/middlewares"
+	"fmt"
+	"os"
 )
 
 func Serve() {
 	cnf := config.GetConfig()
 
+	dbCon, err := db.NewConnection()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	productRepo := repo.NewProductRepo()
-	userRepo := repo.NewUserRepo()
+	userRepo := repo.NewUserRepo(dbCon)
 
 	middlewares := middleware.NewMiddlewares(cnf)
 
